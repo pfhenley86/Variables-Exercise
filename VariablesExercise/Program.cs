97999efc5ac981a45922d269ddbcc2516fe93796
Ford Henley <info@fordswebhelp.com>
2025-08-14 11:50:03 -0500

    Created Variables and made a story to print out the variables to the console.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string jobTitle = "Associate Software Engineer";
+            int age = 42;
+            char officeFloor = '5';
+            bool isAgoodCoder = true;
+            double trueCodersGradeAverage = 86.7;
+            decimal newSalary = 123.456M;
+            
+            Console.WriteLine($"Bob graduated from the TrueCoders Bootcamp on his birthday and is now {age} years old.\nWhen he graduated his grade average was {trueCodersGradeAverage}.\nHis instructor was asked if Bob was a good coder as a job reference and he said {isAgoodCoder}.\nHe got a new job and his new job title is {jobTitle}.\nHe works at NASA on the {officeFloor} floor and his new salary is ${newSalary} thousand dollars a year. ");
         }
     }
 }
